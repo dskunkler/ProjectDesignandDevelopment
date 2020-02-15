3330817bb456039c5a4058518a7c4e68f20306e5
Daniel Kunkler <kunkl007@umn.edu>
2020-02-15 16:02:37 -0600

    add(lab07): Adding lab 7 deliverables

---
+++ labs/lab07_style_doxy/src/mainpage.h
@@ -0,0 +1,17 @@
+/**
+ * @file mainpage.h
+ *
+ * @copyright 2020 Daniel Kunkler
+ * , All rights reserved.
+ */
+#ifndef SRC_MAINPAGE_H_
+#define SRC_MAINPAGE_H_
+/*! \mainpage Passenger Index Page For Lab 7
+ *
+ * \section intro_sec Introduction
+ *
+ * The following pages contain UML documentation for passenger and
+ * passenger_factory.
+ *
+ */
+#endif  // SRC_MAINPAGE_H_
--- labs/lab07_style_doxy/src/passenger.h
+++ labs/lab07_style_doxy/src/passenger.h
@@ -1,18 +1,84 @@
-#ifndef PASSENGER_H
-#define PASSENGER_H
+/**
+ * @file passenger_factory.h
+ *
+ * @copyright 2019 3081 Staff with ammendments by Daniel Kunkler
+ * , All rights reserved.
+ */
+#ifndef SRC_PASSENGER_H_
+#define SRC_PASSENGER_H_
 
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
 #include <iostream>
 #include <string>
 
-class Passenger { // : public Reporter {
+/*******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
+/**
+ * @brief The main class for passengers.
+ *
+ * Calls to \ref Passenger generate a new Passenger instant default name is
+ * "Nobody" and destination is NULL.
+ * Calls to \ref Update increments wait_at_stop_.
+ * Calls to \ref GetOnBus sets time of bus to 1.
+ * Calls to \ref GetTotalWait returns wait_at_stop_.
+ * Calls to \ref IsOnBus returns bool false.
+ * Calls to \ref GetDestination returns destination_stop_id_.
+ * Calls to \ref Report outputs name, destination, total wait, time at stop
+ * and time on bus.
+ */
+class Passenger {  // : public Reporter {
  public:
-  Passenger(int = -1, std::string = "Nobody");
+ /**
+  * @brief Generation of a passenger with name Nobody.
+  *
+  * @param[in] destination_stop_id destination stop id
+  * @param[in] name_ name of passenge
+  * Sets wait_at_stop_ to 0, time_on_bus_ to 0, and the id_ to the count then
+  * increments count
+  *
+  * @return Passenger object with all fields filled.
+  */
+  explicit Passenger(int = -1, std::string = "Nobody");
+  /**
+   * @brief Updates wait at stop
+   *
+   * Increments wait_at_stop_ by 1
+   */
   void Update();
+   /**
+    * @brief Sets time on bus to 1.
+    *
+    */
   void GetOnBus();
+  /**
+   * @brief Gets total wait time.
+   *
+   * @return wait_at_stop_ as int
+   */
   int GetTotalWait() const;
+  /**
+   * @brief Returns bool of false
+   *
+   * @return false
+   */
   bool IsOnBus() const;
+  /**
+   * @brief Gets destination id of passenger
+   *
+   * @return destination_stop_id_
+   */
   int GetDestination() const;
+  /**
+   * @brief Prints passenger information
+   *
+   * Prints Name of passenger, destination, total wait time, waits at stop
+   * and time on bus.
+   */
   void Report() const;
+
  private:
   std::string name_;
   int destination_stop_id_;
@@ -20,6 +86,5 @@ class Passenger { // : public Reporter {
   int time_on_bus_;
   int id_;
   static int count_;  // global count, used to set ID for new instances
-  
 };
-#endif
+#endif  // SRC_PASSENGER_H_
