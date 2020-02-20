785208e89517834f24b79c5bc7d6982f3c0e6148
Daniel Kunkler <kunkl007@umn.edu>
2020-02-20 13:10:58 -0600

    Adding changes for exercise 02

--- exercises/ex02_inheritance/composed.h
+++ exercises/ex02_inheritance/composed.h
@@ -4,21 +4,21 @@
 using std::cout;
 using std::endl;
 
-class ComposedClass {
+class ComposedClass{
 public:
   void Print() {
-    cout << "in ComposedClass... " << endl;
-    //cout << "var_a_ " << var_a_ << endl;
-    //cout << "var_b_ " << var_b_ << endl;
+   // cout << "in ComposedClass... " << endl;
+   // cout << "var_a_ " << var_a_ << endl;
+   // cout << "var_b_ " << var_b_ << endl;
     //cout << "object var_a_ " << object_.var_a_ << endl;
     //cout << "object var_b_ " << object_.var_b_ << endl;
     object_.Print();
   }
 protected:
-  // int var_b_;
+   int var_b_;
 private:
   ObjectClass object_;
-  // int var_a_;
+   int var_a_;
 };
 
 #endif
--- exercises/ex02_inheritance/object.h
+++ exercises/ex02_inheritance/object.h
@@ -7,6 +7,7 @@ using std::cout;
 using std::endl;
 
 class ObjectClass {
+	//friend class ComposedClass; 
 public:
   ObjectClass() : var_a_(10), var_b_(15) {}
   ObjectClass(int a, int b) : var_a_(a), var_b_(b) {}
@@ -15,7 +16,7 @@ public:
     cout << "priv, prot =  " << var_a_ << ' '<< var_b_ << endl;
   }
 private:
-  int var_a_;
+	int var_a_;
 protected:
   int var_b_;
 };
