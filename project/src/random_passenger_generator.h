3fc8950c2cc664071a79bc7ee27fc2055ef8669a
Daniel Kunkler <kunkl007@umn.edu>
2020-03-02 13:37:43 -0600

    WHY WONT THIS WORK

--- project/src/random_passenger_generator.h
+++ project/src/random_passenger_generator.h
@@ -1,7 +1,7 @@
 /**
  * @file  random_passenger_generator.h
  *
- * @copyright 2019 3081 Staff, All rights reserved.
+ * @copyright 2019 3081 Staff and D. Kunkler, All rights reserved.
  */
 
 #ifndef SRC_RANDOM_PASSENGER_GENERATOR_H_
