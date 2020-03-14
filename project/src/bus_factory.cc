54d5367a1527dbe38514d3d8f5cd83e94ee15789
Daniel Kunkler <kunkl007@umn.edu>
2020-03-14 15:08:45 -0500

    refactor(visualization_simulator/bus): fix#5 added bus factory
    I added a bus factory which randomly generates a bus of size 30, 60, 90.
    I didn't give them their own classes since it seems redundant to create 6 new files to simply pass a different parameter,
    however if we decide to make any other alterations to the bus, that will become neccessary.

---
+++ project/src/bus_factory.cc
@@ -0,0 +1,36 @@
+/**
+* @file bus_factory.cc
+*
+* @copyright 2020 Daniel Kunkler, all rights reserved.
+*/
+#include <vector>
+#include <random>
+#include <string>
+#include "src/bus_factory.h"
+
+std::random_device dev1;
+std::mt19937 rng(dev1());
+std::uniform_int_distribution<std::mt19937::result_type> dist1(1,3);
+int size = dist1(rng);
+
+/*******************************************************************************
+ * Member Functions
+ ******************************************************************************/
+Bus *BusFactory::Generate(std::string id, Route * out, Route *in) {
+
+
+  switch(size) {
+    case 1:
+      return new Bus(id, out, in, 30, 1);
+      break;
+    case 2:
+      return new Bus(id, out, in, 60, 1);
+      break;
+    case 3:
+      return new Bus(id, out, in, 90, 1);
+
+    default:
+      std::cout << "ERROR e NOT BETWEEN 1 AND 3!!!\n";
+  }
+  return NULL;
+}
---
+++ project/src/bus_factory.h
@@ -0,0 +1,19 @@
+/**
+*
+* @file bus_factory.h
+*
+* @copyright 2020 Daniel Kunkler, All rights reserved
+*/
+
+#ifndef SRC_BUS_FACTORY_H_
+#define SRC_BUS_FACTORY_H_
+
+#include <string>
+
+#include "src/bus.h"
+
+class BusFactory {
+ public:
+  static Bus *Generate(std::string name, Route*, Route*);
+};
+#endif  // SRC_BUS_FACTORY_H_
