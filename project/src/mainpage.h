c5e3b9ae62089d77eb34ab7da25b885a9e2b86cc
Daniel Kunkler <kunkl007@umn.edu>
2020-03-20 00:38:51 -0500

    docs(mainpage.h): Working on documentation for final deliverable
    I added how to get, construct, and run the simulation.

--- project/src/mainpage.h
+++ project/src/mainpage.h
@@ -12,5 +12,43 @@
  *
  * The following pages contain UML documentation for Bus System Simulator
  *
+ * How to obtain the code:
+ * 	To obtain the code navigate to the website http://github.umn.edu/umn-csci-3081-s20/repo-kunkl007
+ * Download the files and unzip them, then navigate to project. Alternatively, create a folder and navigate to it through the command line and type
+ * "git clone http://github.umn.edu/umn-csci-3081-s20/repo-kunkl007.git".
+ * This will create a copy of the entire file structure and you will have to navigate to the "project" folder to see the project we will be referring to for the rest of this document.
+ * 	To compile the code, further navigate to the src folder by type "cd src/" in the command line. Here you will want to type and enter "make clean" then "make". This should compile the project for you to be 
+ * able to run. Type "cd .." to navigate back to project. From here you will want to type "./build/bin/vis_sim 8123". The command line should print "starting server...". At this point you'll want
+ * to open your internet browser and enter "http://127.0.0.1:8123/web_graphics/project.html"
+ * At this point the command line should say "Connection established". From here you can choose how many steps you want to run of the simulation with the top scroll bar. The bottom bars let you
+ * choose how many steps you want to wait in between spawning new busses.
+ *
+ * 
+ *
+ *
+ * 
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
 #endif  // SRC_MAINPAGE_H_
