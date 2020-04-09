040173a29dab86a535ec0d7671bf4dbec2e6c070
Daniel Kunkler <kunkl007@umn.edu>
2020-04-09 12:31:45 -0500

    docs(src/afternoon_strategy): fix#07 documented functions
    I added all the doxygen documentation for afternoon_strategy

--- project/src/afternoon_strategy.h
+++ project/src/afternoon_strategy.h
@@ -15,9 +15,32 @@
 /*******************************************************************************
  * Class Definitions
  ******************************************************************************/
+ /**
+  * @brief The AfternoonStrategy Class.
+  *
+  * Calls to \ref AfternoonStrategy create a new AfternoonStrategy instance
+  * that is inherited from BusStrategy inherits state from BusStrategy starting
+  * at 0.
+  * Calls to \ref DeployStrat deploy Busses according to the Afternoon
+  * strategy, that is small, regular, large, sequentially.
+  */
 class AfternoonStrategy : public BusStrategy {
  public:
+   /**
+    * @brief Generator for AfternoonStrategy
+    *
+    * @return AfternoonStrategy instance.
+    */
   AfternoonStrategy();
+
+  /**
+   * @brief Generates a small,then a regular, then a large bus.
+   * @param[in] id Bus name.
+   * @param[in] out Outbound Route.
+   * @param[in] in Inbound Route.
+
+   * @return Bus object with all fields filled
+   */
   Bus *DeployStrat(std::string id, Route * out, Route *in);
 };
 #endif  // SRC_AFTERNOON_STRATEGY_H_
