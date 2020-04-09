5bdb361c9b6254f82f3c898f147d773463a30d74
Daniel Kunkler <kunkl007@umn.edu>
2020-04-09 13:27:45 -0500

    docs(alternative_strategy0): fix#07 alternative_strategy
    I did all the doxygen documentation for alternative_strategy

--- project/src/alternative_strategy.h
+++ project/src/alternative_strategy.h
@@ -15,10 +15,33 @@
 /*******************************************************************************
  * Class Definitions
  ******************************************************************************/
+
+ /**
+  * @brief The AlternativeStrategy Class.
+  *
+  * Calls to \ref AlternativeStrategy create a new AlternativeStrategy instance
+  * that is inherited from BusStrategy inherits state from BusStrategy starting
+  * at 0.
+  * Calls to \ref DeployStrat deploy Busses according to the Alternative
+  * strategy, that is small busses only.
+  */
 class AlternativeStrategy : public BusStrategy {
  public:
-  // the alternative strategy only puts out small busses so state is irrelevant
+   /**
+    * @brief Generator for AlternatStrategy
+    *
+    * @return AlternatStrategy instance.
+    */
   AlternativeStrategy();
+
+  /**
+   * @brief Generates a small bus only.
+   * @param[in] id Bus name.
+   * @param[in] out Outbound Route.
+   * @param[in] in Inbound Route.
+
+   * @return Bus object with all fields filled
+   */
   Bus *DeployStrat(std::string id, Route * out, Route *in) override;
 };
 #endif  // SRC_ALTERNATIVE_STRATEGY_H_
