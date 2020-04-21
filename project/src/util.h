57b9366eb744bacc37ff761a98792bbde281bcd6
Daniel Kunkler <kunkl007@umn.edu>
2020-04-20 19:41:05 -0500

    feat(src): fix#23 added util.h
    I added util.h and declared the function within it as static so we can use it without explicitly creating a util object.
    I have yet to define it though.

---
+++ project/src/util.h
@@ -0,0 +1,24 @@
+/**
+* @file util.h
+*
+* @copyright 2020 Daniel Kunkler, All rights reserved.
+*/
+#ifndef SRC_UTIL_H_
+#define SRC_UTIL_H_
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <string>
+#include <vector>
+#include <sstream>
+/*******************************************************************************
+ * Class Definitions
+ ******************************************************************************/
+class Util {
+ public:
+  static void processOutput(ostringstream);
+  // TODO@ME fill in the definition.
+};
+
+#endif  // SRC_UTIL_H_
